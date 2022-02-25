01f2cf32da2b67033c19cfa6e2db871610944ac7
Paul <paulmichaelhardman@gmail.com>
2022-02-25 14:10:15 +0100

    MVP produced

--- morse.js
+++ morse.js
@@ -29,35 +29,32 @@ const cleanInputString = (string) => {
   return cleanString
 };
 
-
 const cleanEnglishStringToMorse = (string) => {
   // This will look through every item in the string and map it based on the value
   // split the string into each character, the use map, then use join
   const stringInput = cleanInputString(string);
   // split the string
   const stringIn = stringInput.split('');
-  
   // translate the string using map function 
   const translatedArray = stringIn.map(letter => {
-    // return sweetItem+"A"
     if (dictionary[letter] !== undefined) {
       return dictionary[letter];
     } else {
       return "/"
     }
   })
-
+// return the rejoined string
   return translatedArray.join("")
-  // now we need to rejoin the string with a join function
 }
 
 // write the function that happens when you press the button
 const translateMorsetoEnglishFunction = () => {
   // use .value to get the value out of the system 
-  console.log(englishToTranslate.value)
+  
+
   // now we want to 1) run the translate function on our string
   // const textToDisplay = translateFunctionIneedtowrite();
-  translationToDisplay.innerText = "The string result variable will be assigned here"
+  translationToDisplay.innerText = cleanEnglishStringToMorse(englishToTranslate.value);
 }
 
 // On button press we run our translate function 
