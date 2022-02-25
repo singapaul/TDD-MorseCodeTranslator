a8f4a0707495d9d644a0f9a8fed88ad9b1a717fd
Paul <paulmichaelhardman@gmail.com>
2022-02-25 10:31:00 +0100

    created base test files

---
+++ morse.test.js
@@ -0,0 +1,39 @@
+import {expect, it, describe} from "@jest/globals";
+//Write test for morse code function 
+
+// import {multiply} from "./sums";
+
+// describe("Multiply function tests", () => {
+//     it("Multiplies 5 and 2 to equal 10", ()=> {
+//         const result = multiply(5,2);
+//         expect(result).toBe(10)
+//     });
+//     it("Multiplies 10 and 10 to equal 100", ()=> {
+//         const result = multiply(10,10);
+//         expect(result).toBe(100)
+//     });
+
+// })
+
+
+
+
+describe("", () => {
+  //Write our postive tests: 
+
+  it("", () => {
+    //Write tests in here 
+  })
+  it("", () => {
+    //Write tests in here 
+  })
+
+  //Write negative:
+  //When we give a param not a coach name 
+  it("", () => {
+    //write our tests here 
+  })
+  it("", () => {
+    //write our tests here 
+  })
+})
