a8945a359bfebc86011ad52f8a72b6b1c6e41f34
Paul <paulmichaelhardman@gmail.com>
2022-03-06 10:45:58 +0000

    final page

--- function.js
+++ function.js
@@ -1,5 +1,7 @@
 import { dictionary } from "./objects/dictionary.js";
 
+import { MORSE_CODE } from "./objects/dictionary.js";
+
 // This line will remove any spaces and replace with a /
 export const spaceToString = (string) => {
   // a single s on it's own will pick up all whitespace - whereas /s/s will only pickup when there is more than 2 spaces
@@ -31,5 +33,28 @@ export const cleanEnglishStringToMorse = (string) => {
     }
   });
   // join the letters back into single string and return result
-  return translatedArray.join("");
+  return translatedArray.join(" ");
+};
+
+// the functions for the morse code to english translation
+
+export const cleanMorseToEnglish = (string) => {
+  return decodeMorse(string);
+};
+
+
+export const decodeMorse = (morseCode) => {
+  let words = morseCode.split("/");
+  let letters = words.map((w) => w.split(" "));
+  let decoded = [];
+
+  for (var i = 0; i < letters.length; i++) {
+    decoded[i] = [];
+    for (var x = 0; x < letters[i].length; x++) {
+      if (MORSE_CODE[letters[i][x]]) {
+        decoded[i].push(MORSE_CODE[letters[i][x]]);
+      }
+    }
+  }
+  return decoded.map((arr) => arr.join("")).join(" ");
 };
